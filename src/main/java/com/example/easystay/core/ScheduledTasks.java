43b511166f938cd5a77f2040f4f2e481a75b66b9
hazartamer <hazartamer23@gmail.com>
2024-06-17 03:35:23 +0300

    ScheduledTask eklendi -@Scheduled anatasyonu ilk defa kullandık -Misafir çıkış yaptığında otomatik olarak 'cleaning' olarak set edildi ve 30 dakikanın ardından 'available' olarak güncellendi

---
+++ src/main/java/com/example/easystay/core/ScheduledTasks.java
@@ -0,0 +1,52 @@
+package com.example.easystay.core;
+
+import com.example.easystay.core.exceptionhandling.exception.types.BusinessException;
+import com.example.easystay.model.entity.Reservation;
+import com.example.easystay.model.entity.Room;
+import com.example.easystay.model.enums.Status;
+import com.example.easystay.repository.ReservationRepository;
+import com.example.easystay.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class ScheduledTasks {
+    private final ReservationRepository reservationRepository;
+    private final RoomRepository roomRepository;
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    @Scheduled(cron = "0 0 12 * * *") // 5 saniyede bir çalışır
+    public void updateHotelStatus(){
+        LocalDate today = LocalDate.now();
+        List<Reservation> reservation = reservationRepository.findAll();
+        for(Reservation reservation1 : reservation){
+            if(reservation1.getCheckOutDate().isAfter(today)){
+                Room room = reservation1.getRoom();
+                room.setStatus(Status.CLEANING);
+                roomRepository.save(room);
+                scheduleRoomAvailabilityUpdate(room.getId(),30);
+            }
+        }
+
+    }
+    private void scheduleRoomAvailabilityUpdate(Long roomId, int delayInMinutes) {
+        scheduler.schedule(() -> markRoomAsAvailable(roomId), delayInMinutes, TimeUnit.MINUTES);
+    }
+    private void markRoomAsAvailable(Long roomId) {
+        Room room = roomRepository.findById(roomId).orElseThrow(() -> new BusinessException("Room not found"));
+        room.setStatus(Status.AVAILABLE);
+        roomRepository.save(room);
+        System.out.println("Oda " + roomId + " AVAILABLE olarak güncellendi: " + LocalDateTime.now());
+    }
+
+
+
+}
--- src/main/java/com/example/easystay/repository/RoomRepository.java
+++ src/main/java/com/example/easystay/repository/RoomRepository.java
@@ -4,6 +4,7 @@ package com.example.easystay.repository;
 import com.example.easystay.model.entity.Room;
 import com.example.easystay.model.enums.RoomType;
 
+import com.example.easystay.model.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,6 +13,9 @@ import java.util.List;
 public interface RoomRepository extends JpaRepository<Room, Long> {
     @Query("SELECT r FROM rooms r WHERE r.roomType = :roomType AND r.status = com.example.easystay.model.enums.Status.AVAILABLE")
     List<Room> findByRoomType(RoomType roomType);
+
+    List<Room> findByStatus(Status status);
+
 
 
 
